d9f88bd1a4cf3bacbc27a554ca5032ad4fd2300b
JanuszMarek <janusz.marek92@gmail.com>
2019-02-05 19:55:44 +0100

    Create HTML Helpers according to DRY, change names of partial views,.

---
+++ FilmoweJanusze/Helpers/BeginHeaderHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmoweJanusze.Helpers
+{
+    public static class BeginHeaderHelper
+    {
+        
+        public static IDisposable BeginHeader(this HtmlHelper htmlHelper, string MainTitle)
+        {
+            return BeginHeader(htmlHelper, MainTitle, String.Empty);
+        }
+
+        public static IDisposable BeginHeader(this HtmlHelper htmlHelper, string MainTitle, string SubTitle)
+        {
+            htmlHelper.ViewContext.Writer.Write(
+                "<div class=\"page-header well-jm realative\"> " +
+                "<div class=\"col-xs-10 col-sm-11\">" +
+                "<h3>" + MainTitle + "</h3>");
+
+            if(!String.IsNullOrEmpty(SubTitle))
+            {
+                htmlHelper.ViewContext.Writer.Write("<h4>" + SubTitle + "</h4>");
+            }
+
+            htmlHelper.ViewContext.Writer.Write(
+                "</div>"
+                );
+            return new BeginHeaderClass(htmlHelper);
+        }
+
+        class BeginHeaderClass : IDisposable
+        {
+            private HtmlHelper htmlHelper;
+
+            public BeginHeaderClass(HtmlHelper htmlHelper)
+            {
+                this.htmlHelper = htmlHelper;
+            }
+
+            public void Dispose()
+            {
+                this.htmlHelper.ViewContext.Writer.Write(
+                    "<div class=\"clearfix\"></div></div>"
+                    );
+            }
+        }
+
+    }
+
+
+}
---
+++ FilmoweJanusze/Helpers/BeginPanelHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace FilmoweJanusze.Helpers
+{
+    public static class BeginPanelHelper
+    {
+        public static IDisposable BeginPanel(this HtmlHelper htmlHelper, string Title, string id, string classname, HelperResult helperResult = null)
+        {
+            string temp = String.Empty;
+
+            htmlHelper.ViewContext.Writer.Write(
+                "<div class=\"panel-group nopadding\">" +
+                "<div class=\"panel panel-default\">" +
+                "<div class=\"panel-heading container-fluid\">");
+
+            if (helperResult != null)
+            {
+                htmlHelper.ViewContext.Writer.Write(
+                    "<div class=\"col-xs-10 col-sm-11 nopadding\">"
+                    );
+            }
+
+            htmlHelper.ViewContext.Writer.Write(
+                "<h4 class=\"panel-title\">" +
+                "<a data-toggle=\"collapse\" href=\"#" + id + "\" class=\"panel-link\">" +
+                Title +
+                "</a></h4></div>");
+
+            if (helperResult != null)
+            {
+                htmlHelper.ViewContext.Writer.Write(
+                    
+                    helperResult.ToString() +
+                    "</div>"
+                    );
+            }
+
+            htmlHelper.ViewContext.Writer.Write(
+                "<div id=\"" + id + "\" class=\"panel-collapse collapse " + classname + "\">" +
+                "<div class=\"panel-body nopadding\">"
+                );
+
+            return new BeginPanelClass(htmlHelper);
+        }
+
+        class BeginPanelClass : IDisposable
+        {
+            private HtmlHelper htmlHelper;
+
+            public BeginPanelClass(HtmlHelper htmlHelper)
+            {
+                this.htmlHelper = htmlHelper;
+            }
+
+            public void Dispose()
+            {
+                this.htmlHelper.ViewContext.Writer.Write(
+                    "</div></div></div><div style=\"clear: both\"></div></div>"
+                    );
+            }
+        }
+    }
+}
